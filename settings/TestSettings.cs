7a06c1c4469f5973b19b9d3aceba809cd6ea7016
stepaandr013 <stepa.andr@gmail.com>
2022-08-26 21:29:42 +0300

    Initial commit V3

---
+++ settings/TestSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_999.md.settings
+{
+    internal class TestSettings
+    {
+        public static readonly string URL = "https://999.md/ru/";
+        public static readonly string SEARCH_ITEM = "ford fusion";
+    }
+}
